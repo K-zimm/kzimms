3d726a80580cea6425dd3c2c93bfab85a4e98a98
Kyle Zimmer <kylezimmer2@gmail.com>
2020-05-06 21:12:39 -0500

    More form troubleshooting

--- src/components/splash-page.js
+++ src/components/splash-page.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { navigate } from "gatsby-link"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Image from "gatsby-image"
 import SEO from "../components/seo"
@@ -7,6 +8,12 @@ import ProfilePic from "../images/Profile-pic-outerglow.png"
 import Logo from "../../content/assets/logo-underline-white.svg"
 
 import "../styles/main.scss"
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
 const SplashPage = () => {
   const data = useStaticQuery(graphql`
@@ -20,6 +27,27 @@ const SplashPage = () => {
       }
     }
   `)
+
+  const [state, setState] = React.useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
 
   return (
     <div className="splash-page">
@@ -62,10 +90,12 @@ const SplashPage = () => {
             <form
               name="splash-newsletter"
               method="post"
-              netlify
-              netlify-honeypot="bot-field"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
               action="/thank-you/launch"
+              onSubmit={handleSubmit}
             >
+              <input type="hidden" name="form-name" value="splash-newsletter" />
               <p>
                 <input type="hidden" name="bot-field" />
               </p>
