443cf1200723f25bbb9f2617aa362e723e0bb832
Kyle Zimmer II <24055429@nwtc.edu>
2020-05-21 19:14:56 -0500

    Styled launch thank you page and added space shuttle animation

--- src/pages/thank-you/launch.js
+++ src/pages/thank-you/launch.js
@@ -12,17 +12,24 @@ const ThankyouLaunch = ({ data }) => {
       <div className="thankyou-graphic">
         <SpaceShuttle />
       </div>
-      <div className="thankyou-message">
-        <h1>Thank You for subscribing to my newsletter!</h1>
-        <p>I hope you will find my journey interesting and gain some value from it! I am always open to feedback. Please don't hesitate to ask me questions, suggest content, or offer constructive criticism.</p>
-        <p>
-          <em>You can unsubscribe anytime by clicking the link at the bottom of the emails.</em>
-        </p>
-      </div>
-      <div className="thankyou-social">
-        <h2>Show Me Some Social Love</h2>
-        <SocialLinks />
-      </div>
+        <div className="thankyou-message">
+          <h1>Thank You for subscribing to my newsletter!</h1>
+          <p>
+            I hope you will find my journey interesting and gain some value from
+            it! I am always open to feedback. Please don't hesitate to ask me
+            questions, suggest content, or offer constructive criticism.
+          </p>
+          <p>
+            <em>
+              You can unsubscribe anytime by clicking the link at the bottom of
+              the emails.
+            </em>
+          </p>
+        </div>
+        <div className="thankyou-social">
+          <h2>Show Me Some Social Love</h2>
+          <SocialLinks />
+        </div>
     </div>
   )
 }
