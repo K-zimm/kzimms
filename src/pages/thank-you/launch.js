a3e9d9fd23814a68e6d53db4b51c4d564e799cfb
Kyle Zimmer <kylezimmer2@gmail.com>
2020-11-10 21:56:36 -0600

    hubspot widget test

--- src/pages/thank-you/launch.js
+++ src/pages/thank-you/launch.js
@@ -4,23 +4,21 @@ import SEO from "../../components/seo"
 import Layout from "../../components/layout"
 import SpaceShuttle from "../../../content/assets/blast-off.svg"
 
-
-
 const ThankyouLaunch = ({ data }) => {
   return (
     <Layout className="container thank-you">
       <div className="thankyou-page">
         <SEO title="Thank You!" />
-
         <div className="thankyou-graphic">
           <SpaceShuttle />
         </div>
         <div className="thankyou-message">
-          <h1>Thank You for requesting a discovery call!</h1>
-          <p>
-            I will be contacting you shorty to schedule the call! Please prepare with information and materials that will help me understand what type of website or application you are looking for.
-          </p>
+          <h1>Please select a meeting time.</h1>
         </div>
+        <div
+          className="meetings-iframe-container"
+          data-src="https://meetings.hubspot.com/kyle532?embed=true"
+        ></div>
       </div>
     </Layout>
   )
